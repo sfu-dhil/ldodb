c3ba5cb65c7e0624c04c0f6b0a0a45d9685c2672
Erik Hanson <ewhanson@sfu.ca>
2018-03-02 11:31:47 -0800

    Add Features page with descriptive bibliography info

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -7,6 +7,11 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+use AppBundle\Entity\BindingFeature;
+use AppBundle\Entity\MapSize;
+use AppBundle\Entity\MapType;
+use AppBundle\Entity\PlateType;
+
 class DefaultController extends Controller {
 
     /**
@@ -17,6 +22,26 @@ class DefaultController extends Controller {
         return [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')) . DIRECTORY_SEPARATOR,
         ];
+    }
+
+    /**
+     * @Route("/features", name="features")
+     * @Template()
+     */
+    public function featuresAction(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+        $qb = $em->createQueryBuilder();
+        $bindingFeatures = $qb->select('bf')->from(BindingFeature::class, 'bf')->orderBy('bf.bindingFeature', 'ASC')->getQuery()->execute();
+        $mapSizes = $qb->select('ms')->from(MapSize::class, 'ms')->orderBy('ms.mapSize', 'ASC')->getQuery()->execute();
+        $mapTypes = $qb->select('mt')->from(MapType::class, 'mt')->orderBy('mt.mapType', 'ASC')->getQuery()->execute();
+        $plateTypes = $qb->select('pt')->from(PlateType::class, 'pt')->orderBy('pt.plateType', 'ASC')->getQuery()->execute();
+
+        return array(
+            'bindingFeatures' => $bindingFeatures,
+            'mapSizes' => $mapSizes,
+            'mapTypes' => $mapTypes,
+            'plateTypes' => $plateTypes,
+        );
     }
 
 }
